6c199769a7832a7217cabbdda49d5772b814bca8
jonndoe <qb89@yandex.ru>
2020-06-06 01:43:01 +0300

    added red test_user_can_signup

--- server/taxi/trips/tests/test_http.py
+++ server/taxi/trips/tests/test_http.py
@@ -1,11 +1,21 @@
 # tests/test_http.py
 
+import base64
+import json
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
 PASSWORD = 'pAssw0rd!'
+
+def create_user(username='user@example.com', password=PASSWORD): # new
+    return get_user_model().objects.create_user(
+        username=username,
+        first_name='Test',
+        last_name='User',
+        password=password
+    )
 
 
 class AuthenticationTest(APITestCase):
@@ -23,3 +33,24 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(response.data['username'], user.username)
         self.assertEqual(response.data['first_name'], user.first_name)
         self.assertEqual(response.data['last_name'], user.last_name)
+
+    def test_user_can_log_in(self): # new
+        user = create_user()
+        response = self.client.post(reverse('log_in'), data={
+            'username': user.username,
+            'password': PASSWORD,
+        })
+
+        # Parse payload data from access token.
+        access = response.data['access']
+        header, payload, signature = access.split('.')
+        decoded_payload = base64.b64decode(f'{payload}==')
+        payload_data = json.loads(decoded_payload)
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertIsNotNone(response.data['refresh'])
+        self.assertEqual(payload_data['id'], user.id)
+        self.assertEqual(payload_data['username'], user.username)
+        self.assertEqual(payload_data['first_name'], user.first_name)
+        self.assertEqual(payload_data['last_name'], user.last_name)
+
