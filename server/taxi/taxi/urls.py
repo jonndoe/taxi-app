c505e51b73231f9d0bfa51b12da4b8dc6b0462e9
jonndoe <qb89@yandex.ru>
2020-06-06 01:37:38 +0300

    added sign up ability

--- server/taxi/taxi/urls.py
+++ server/taxi/taxi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from trips.views import SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/sign_up/', SignUpView.as_view(), name='sign_up'),
 ]
--- server/taxi/trips/views.py
+++ server/taxi/trips/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# trips/views.py
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+
+from .serializers import UserSerializer
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
