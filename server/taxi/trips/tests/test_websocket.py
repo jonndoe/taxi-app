b1bff428deecb33fc2b6d837d74e6c34f7807be9
jonndoe <qb89@yandex.ru>
2020-06-07 02:34:35 +0300

    added test_can_send_and_receive_broadcast_messages test

--- server/taxi/trips/tests/test_websocket.py
+++ server/taxi/trips/tests/test_websocket.py
@@ -4,7 +4,7 @@ from channels.testing import WebsocketCommunicator
 import pytest
 
 from taxi.routing import application
-
+from channels.layers import get_channel_layer
 
 TEST_CHANNEL_LAYERS = {
     'default': {
@@ -23,4 +23,38 @@ class TestWebSocket:
         )
         connected, _ = await communicator.connect()
         assert connected is True
+        await communicator.disconnect()
+
+
+    async def test_can_send_and_receive_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
+
+    async def test_can_send_and_receive_broadcast_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('test', message=message)
+        response = await communicator.receive_json_from()
+        assert response == message
         await communicator.disconnect()
