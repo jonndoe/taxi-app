06a5a2b7fa067f1fe69b620f7fbcfc92a224508e
jonndoe <qb89@yandex.ru>
2020-06-08 00:46:06 +0300

    added ability for drivers to join drivers pool upon subscription.

--- server/taxi/trips/consumers.py
+++ server/taxi/trips/consumers.py
@@ -1,17 +1,27 @@
 # trips/consumers.py
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.db import database_sync_to_async
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
     groups = ["test"]
+
+    @database_sync_to_async
+    def _get_user_group(self, user):
+        return user.groups.first().name
 
     async def connect(self):
         user = self.scope["user"]
         if user.is_anonymous:
             await self.close()
         else:
-            await self.channel_layer.group_add(group="test", channel=self.channel_name)
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_add(
+                    group="drivers",
+                    channel=self.channel_name
+                )
 
             await self.accept()
 
@@ -21,8 +31,13 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
         )
 
     async def disconnect(self, code):
-
-        await self.channel_layer.group_discard(group="test", channel=self.channel_name)
+        user = self.scope['user']
+        user_group = await self._get_user_group(user)
+        if user_group == 'driver':
+            await self.channel_layer.group_discard(
+                group="drivers",
+                channel=self.channel_name
+            )
 
         await super().disconnect(code)
 
--- server/taxi/trips/tests/test_websocket.py
+++ server/taxi/trips/tests/test_websocket.py
@@ -11,14 +11,29 @@ from django.contrib.auth import get_user_model
 from channels.db import database_sync_to_async
 from rest_framework_simplejwt.tokens import AccessToken
 
+from django.contrib.auth.models import Group
+
 TEST_CHANNEL_LAYERS = {
     "default": {"BACKEND": "channels.layers.InMemoryChannelLayer",},
 }
 
 
 @database_sync_to_async
-def create_user(username, password):
-    user = get_user_model().objects.create_user(username=username, password=password)
+def create_user(
+        username,
+        password,
+        group='rider',
+):
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password,
+    )
+
+    # Create user group.
+    user_group, _ = Group.objects.get_or_create(name=group) # new
+    user.groups.add(user_group)
+    user.save()
+
     access = AccessToken.for_user(user)
     return user, access
 
@@ -76,3 +91,24 @@ class TestWebSocket:
         communicator = WebsocketCommunicator(application=application, path="/taxi/")
         connected, _ = await communicator.connect()
         assert connected is False
+
+    async def test_join_driver_pool(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+            'test.user@example.com', 'pAssw0rd', 'driver'
+        )
+        communicator = WebsocketCommunicator(
+            application=application,
+            path=f'/taxi/?token={access}'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('drivers', message=message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
+
