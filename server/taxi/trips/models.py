7a0e53dba16c3f00a7925d69a2b1db86a1d83a75
jonndoe <qb89@yandex.ru>
2020-06-09 01:10:36 +0300

    added ui support

--- server/taxi/trips/models.py
+++ server/taxi/trips/models.py
@@ -8,7 +8,10 @@ from django.conf import settings
 
 
 class User(AbstractUser):
-    pass
+    @property
+    def group(self):
+        groups = self.groups.all()
+        return groups[0].name if groups else None
 
 
 class Trip(models.Model):  # new
--- server/taxi/trips/views.py
+++ server/taxi/trips/views.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Trip
 from .serializers import UserSerializer, LogInSerializer, UserSerializer, TripSerializer
-
+from django.db.models import Q
 
 class SignUpView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
@@ -21,5 +21,15 @@ class TripView(viewsets.ReadOnlyModelViewSet):
     lookup_field = "id"
     lookup_url_kwarg = "trip_id"
     permission_classes = (permissions.IsAuthenticated,)
-    queryset = Trip.objects.all()
+
     serializer_class = TripSerializer
+
+    def get_queryset(self): # new
+        user = self.request.user
+        if user.group == 'driver':
+            return Trip.objects.filter(
+                Q(status=Trip.REQUESTED) | Q(driver=user)
+            )
+        if user.group == 'rider':
+            return Trip.objects.filter(rider=user)
+        return Trip.objects.none()
