eb3f5406b7ea3d43b5650a69aba7a2677719a3f7
jonndoe <qb89@yandex.ru>
2020-06-10 00:39:48 +0300

    cypress tests added

---
+++ client/cypress/integration/navigation.spec.js
@@ -0,0 +1,44 @@
+// cypress/integration/navigation.spec.js
+
+describe('Navigation', function () {
+  it('Can navigate to sign up from home', function () {
+    cy.visit('/#/');
+    cy.get('a').contains('Sign up').click();
+    cy.hash().should('eq', '#/sign-up');
+  });
+
+    // new
+  it('Can navigate to log in from home', function () {
+    cy.visit('/#/');
+    cy.get('a').contains('Log in').click();
+    cy.hash().should('eq', '#/log-in');
+  });
+
+  // new
+  it('Can navigate to home from sign up', function () {
+    cy.visit('/#/sign-up');
+    cy.get('a').contains('Home').click();
+    cy.hash().should('eq', '#/');
+  });
+
+  // new
+  it('Can navigate to log in from sign up', function () {
+    cy.visit('/#/sign-up');
+    cy.get('a').contains('Log in').click();
+    cy.hash().should('eq', '#/log-in');
+  });
+
+  // new
+  it('Can navigate to home from log in', function () {
+    cy.visit('/#/log-in');
+    cy.get('a').contains('Home').click();
+    cy.hash().should('eq', '#/');
+  });
+
+  // new
+  it('Can navigate to sign up from log in', function () {
+    cy.visit('/#/log-in');
+    cy.get('a').contains('Sign up').click();
+    cy.hash().should('eq', '#/sign-up');
+  });
+});
--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App () {
       <Route exact path='/' render={() => (
         <div>
           <h1>Taxi</h1>
-          <Link to='/sign-up'>Sign up</Link>
+     <Link to='/sign-up'>Sign up</Link>
           <Link to='/log-in'>Log in</Link>
         </div>
       )} />
