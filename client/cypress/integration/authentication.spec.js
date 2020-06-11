c198356123b51878b1b96d5c203881bca8b443b6
jonndoe <qb89@yandex.ru>
2020-06-11 23:40:42 +0300

    refactoring tests code

--- client/cypress/integration/authentication.spec.js
+++ client/cypress/integration/authentication.spec.js
@@ -2,30 +2,38 @@
 
 
 
+const logIn = () => {
+  const { username, password } = Cypress.env('credentials');
+
+  // Capture HTTP requests.
+  cy.server();
+  cy.route({
+    method: 'POST',
+    url: '**/api/log_in/**',
+    status: 200,
+    response: {
+      'access': 'ACCESS_TOKEN',
+      'refresh': 'REFRESH_TOKEN'
+    }
+  }).as('logIn');
+
+  // Log into the app.
+  cy.visit('/#/log-in');
+  cy.get('input#username').type(username);
+  cy.get('input#password').type(password, { log: false });
+  cy.get('button').contains('Log in').click();
+  cy.wait('@logIn');
+};
+
+
 describe('Authentication', function () {
+
   it('Can log in.', function () {
-
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: '**/api/log_in/**',
-      status: 200,
-      response: {
-        'access': 'ACCESS_TOKEN',
-        'refresh': 'REFRESH_TOKEN'
-      }
-    }).as('logIn');
-
-    cy.visit('/#/log-in');
-    cy.get('input#username').type('gary.cole@example.com');
-    cy.get('input#password').type('pAssw0rd', { log: false });
-    cy.get('button').contains('Log in').click();
-
-    cy.wait('@logIn');
-
+    logIn();
     cy.hash().should('eq', '#/');
     cy.get('button').contains('Log out');
   });
+
   it('Can sign up.', function () {
     cy.visit('/#/sign-up');
     cy.get('input#username').type('gary.cole@example.com');
@@ -44,86 +52,23 @@ describe('Authentication', function () {
     cy.hash().should('eq', '#/log-in');
   });
 
-
   it('Cannot visit the login page when logged in.', function () {
-    const { username, password } = Cypress.env('credentials');
-
-    // Log in.
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: '**/api/log_in/**',
-      status: 200,
-      response: {
-        'access': 'ACCESS_TOKEN',
-        'refresh': 'REFRESH_TOKEN'
-      }
-    }).as('logIn');
-    cy.visit('/#/log-in')
-    cy.get('input#username').type(username)
-    cy.get('input#password').type(password, { log: false })
-    cy.get('button').contains('Log in').click()
-    cy.hash().should('eq', '#/')
-    cy.get('button').contains('Log out')
-    cy.wait('@logIn')
-
+    logIn();
     cy.visit('/#/log-in');
     cy.hash().should('eq', '#/');
   });
 
   it('Cannot visit the sign up page when logged in.', function () {
-    const { username, password } = Cypress.env('credentials');
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: '**/api/log_in/**',
-      status: 200,
-      response: {
-        'access': 'ACCESS_TOKEN',
-        'refresh': 'REFRESH_TOKEN'
-      }
-    }).as('logIn');
-    cy.visit('/#/log-in')
-    cy.get('input#username').type(username)
-    cy.get('input#password').type(password, { log: false })
-    cy.get('button').contains('Log in').click()
-    cy.hash().should('eq', '#/')
-    cy.get('button').contains('Log out')
-    cy.wait('@logIn')
-
+    logIn();
     cy.visit('/#/sign-up');
     cy.hash().should('eq', '#/');
   });
 
-
   it('Cannot see links when logged in.', function () {
-    const { username, password } = Cypress.env('credentials');
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: '**/api/log_in/**',
-      status: 200,
-      response: {
-        'access': 'ACCESS_TOKEN',
-        'refresh': 'REFRESH_TOKEN'
-      }
-    }).as('logIn');
-    cy.visit('/#/log-in')
-    cy.get('input#username').type(username)
-    cy.get('input#password').type(password, { log: false })
-    cy.get('button').contains('Log in').click()
-    cy.hash().should('eq', '#/')
-    cy.get('button').contains('Log out')
-    cy.wait('@logIn')
-
+    logIn();
     cy.get('button#signUp').should('not.exist');
     cy.get('button#logIn').should('not.exist');
   });
-
-
-  
-
-
 
 });
 
